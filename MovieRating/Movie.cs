9380109fd597cfefaa80b025d2ded2c8694a648a
dluttig <dluttig5@live.com>
2020-04-19 15:43:47 +1200

    Add project files.

---
+++ MovieRating/Movie.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieRating
+{
+    class Movie
+    {
+        public string title;
+        public string director;
+        private string rating;
+        public static int movieCount = 0;
+
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating = aRating;
+            movieCount++;
+        }
+
+        public int getMovieCount()
+        {
+            return movieCount;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+            }
+        }
+
+    }
+}
---
+++ MovieRating/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MovieRating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "PG-13");
+            Console.WriteLine(Movie.movieCount);
+            Movie shrek = new Movie("Shrek", "Adam Adamson", "PG");
+            Console.WriteLine(Movie.movieCount);
+
+
+
+            Console.WriteLine(avengers.title + " -- " + avengers.Rating + " -- " + avengers.getMovieCount());
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
